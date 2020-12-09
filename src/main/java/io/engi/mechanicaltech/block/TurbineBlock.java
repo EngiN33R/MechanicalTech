fd10e8c9a027c74345d09ca392e6cc79026bc09e
Ilya Gavrikov <me@engi.io>
2020-12-10 00:09:23 +0300

    Break turbine attachments if turbine is broken

--- src/main/java/io/engi/mechanicaltech/block/TurbineBlock.java
+++ src/main/java/io/engi/mechanicaltech/block/TurbineBlock.java
@@ -2,8 +2,11 @@ package io.engi.mechanicaltech.block;
 
 import io.engi.mechanicaltech.entity.TurbineBlockEntity;
 import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.world.BlockView;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.*;
 
 import javax.annotation.Nullable;
 
@@ -19,5 +22,15 @@ public class TurbineBlock extends HorizontalOrientableBlock implements BlockEnti
 	@Override
 	public BlockEntity createBlockEntity(BlockView view) {
 		return new TurbineBlockEntity(multiplier);
+	}
+
+	@Override
+	public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+		Direction ownFacing = state.get(FACING);
+		BlockPos sidePos = pos.offset(ownFacing);
+		BlockState sideState = world.getBlockState(sidePos);
+		if (sideState.getBlock() instanceof AbstractTurbineAttachmentBlock && sideState.get(FACING) == ownFacing) {
+			world.breakBlock(sidePos, true);
+		}
 	}
 }
